3b945f038ce57c9608e46b4585c7d0fb22cebe6b
Poylar <doaleks8@gmail.com>
2024-05-10 23:13:27 +0800

    test husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,10 +21,6 @@ const AlertDialogDemo = () => {
           <span>qwe</span>
           <AlertDialogHeader>
             <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your account and remove
-              your data from our servers.
-            </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
             <AlertDialogCancel>Cancel</AlertDialogCancel>
